76ce0e434b3f8e0afa20c5654453d481d345d758
NasimBilal <nazznb6@gmail.com>
2021-06-14 20:10:57 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,58 +1,128 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {FormControl, Input, InputLabel, Fab, AppBar, Toolbar, Typography, makeStyles, Button, Dialog, DialogTitle, DialogActions} from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add';
+import DeleteOutlineRoundedIcon from '@material-ui/icons/DeleteOutlineRounded';
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
+import { red } from '@material-ui/core/colors';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      textContent : "",
-      textArray : []
-    }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    margin: 5,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  dialog: {
+    width: 200,
+    backgroundColor: red,
   }
-  textInput=(e)=>this.setState({textContent:e.target.value})
+}));
+
+function App(){
+
+  const [open, setOpen] = useState(false);
+
+  const [input, setInput] = useState('');
+
+  const [add, setAdd] = useState([]);
   
-  addText=(e)=>{
-    this.state.textArray.push(this.state.textContent)
-    this.setState({textArray:this.state.textArray})
+  const [deleted, setDeleted] = useState([]);
+  
+
+  const [done, setDone] = useState([]);
+  
+  const add_item = (event) => {
+    event.preventDefault();
+    setAdd([...add, input])
+    setInput('');
   }
-  textRemove=(i)=>{
+  
+  const remove_task = (w)=>{
     if(!window.confirm("Are you sure you want to delete this?")){
       return;
     }
-    this.state.textArray.splice(i,1);
-    this.setState({textArray:this.state.textArray})
+    setDeleted([...deleted, add.splice(w,1)]);
+    setAdd([...add]);
   }
-  render(){
-    return(
-      <div className="app">
-        <div className="mainHeading">
-          <h1>ToDo List</h1>
-        </div>
-        <div className="subHeading">
-          <br />
-          <h2>  First Get Things Done 🌝 ☕ </h2>
-        </div>
-        <div className="input">
-          <input onChange={this.textInput} type="text" placeholder="🖊️ Add item..." />
-          <i onClick={this.addText} className="fas fa-plus"></i>
-        </div>
-        <div className="todos">
-          {this.state.textArray.map((obj,index)=>{
-            return (
-            <div className="todo">
-              <div className="left">
-                <p>{obj}</p>
-              </div>
-              <div className="right">
-                <i onClick={()=>{this.textRemove(index)}} className="fas fa-times" aria-hidden="true"></i>
-              </div>
-            </div>)
-          })}
-        </div>
+
+  const done_task =(d)=>{
+    setDone([...done, add.splice(d,1)])
+  }
+  
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const classes = useStyles();
+  
+  return(
+    <div className="app">
+      <h2 className="welcome">Welcome</h2>
+      <h3 className="add-your-task">Add Your Tasks...</h3>
+      <form className="form">
+        <FormControl className="form-control">
+          <InputLabel>Add Item</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+          <Fab color="primary" size="medium" aria-label="add" onClick={add_item} type="submit" disabled={!input}>
+            <AddIcon  />
+          </Fab>
+        </FormControl>
+      </form>
+      <div className="box">
+        {
+          add.map((obj,i)=>{
+          return(
+            <div className={classes.root}>
+            <AppBar position="static" color="primary">
+              <Toolbar>
+                <Typography variant="h6" className={classes.title}>
+                  {obj}
+                </Typography>
+                <DoneOutlineIcon className="done" onClick={()=>{done_task(i)}}/>
+                <DeleteOutlineRoundedIcon className="remove" onClick={()=>{remove_task(i)}}/>
+              </Toolbar>
+            </AppBar>
+          </div>
+          )})
+        }
       </div>
-    )    
-  }
+      <Button onClick={handleClickOpen}>Open Completed Tasks</Button>
+      <Dialog open={open} onClose={handleClose}>
+        {
+          done.map((object)=>{
+            return(
+              <DialogTitle className={classes.dialog}><li>{object}</li></DialogTitle>
+            )
+          })
+        }
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">Ok</Button>
+      </DialogActions>
+      </Dialog>
+
+      <Button disabled={!deleted} onClick={handleClickOpen}>Open Cancelled Tasks</Button>
+      <Dialog  disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
+      {
+        deleted.map((object)=>{
+          return(
+            <DialogTitle className={classes.dialog}><li>{object}</li></DialogTitle>
+          )
+        })
+      }
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">Ok</Button>
+      </DialogActions>
+      </Dialog>
+    </div>
+  )
 }
 
 
 export default App;
+
